e82e46e59968d4a1afd5241c34eb5fa04952a4be
Christian Holm Hansen <christian.ahh@gmail.com>
2015-02-01 17:10:20 +0100

    Using Tor browser instead of regular Firefox

--- fingerprint.py
+++ fingerprint.py
@@ -2,14 +2,22 @@ from selenium import webdriver
 import subprocess
 import time
 
-# Change interface to the currently connected
+# Device specific variables
 iface = "en0"
+
+# Firefox profile with Tor
+profile = webdriver.FirefoxProfile()
+profile.set_preference('network.proxy.type', 1)
+profile.set_preference('network.proxy.socks', '127.0.0.1')
+profile.set_preference('network.proxy.socks_port', 9050)
+#profile.set_preference('javascript.enabled', False)
+
 N = int(input("Enter number of sites to visit: "))
 
 # Makes request to the given address
 def getPage(addr):
 	print("Requesting %d: %s" % (i+1, addr))
-	driver = webdriver.Firefox()
+	driver = webdriver.Firefox(profile)
 	driver.get("http://"+addr)
 	driver.quit()
 
